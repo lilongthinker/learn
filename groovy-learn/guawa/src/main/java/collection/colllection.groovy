a86398942dc76eb3a2dc6b43593e4590d4c951bb
lilongthinker <lilongthinker@gmail.com>
2013-03-16 19:41:46 +0800

    添加guava collection的操作

--- groovy-learn/guawa/src/main/java/collection/colllection.groovy
+++ groovy-learn/guawa/src/main/java/collection/colllection.groovy
@@ -1,11 +1,53 @@
 package collection
 
+import com.google.common.base.Function
+import com.google.common.collect.ImmutableListMultimap
+import com.google.common.collect.ImmutableSet
+import com.google.common.collect.Multimaps
+
 /**
- * Created with IntelliJ IDEA.
  * User: dingyue
  * Date: 13-3-14
- * Time: 上午8:33
- * To change this template use File | Settings | File Templates.
  */
-class colllection {
+List<String> strs = new ArrayList<String>();
+strs.add("one");
+strs.add("two")
+strs.add("three")
+strs.add("four")
+strs.add("five")
+strs.add("sixt")
+
+ImmutableSet<String> digits = ImmutableSet.copyOf(strs)
+
+Function<String,Integer> lengthFunction = new Function<String,Integer>(){
+    @Override
+    Integer apply(String input) {
+        return input.length()
+    }
 }
+ImmutableListMultimap<Integer,String> digitsByLength = Multimaps.index(digits,lengthFunction)
+
+println digitsByLength
+
+interface PDO {
+    Long getCustId();
+}
+
+class AdgroupDO implements PDO {
+    Long custId;
+}
+
+List<AdgroupDO> list = [new AdgroupDO(custId: 1),new AdgroupDO(custId: 1),new AdgroupDO(custId: 2),new AdgroupDO(custId: 3)]
+
+Function<PDO,Long> pdoGroupByCustIdFunction = new Function<PDO, Long>() {
+    @Override
+    Long apply(PDO input) {
+        return input.custId
+    }
+}
+
+ImmutableListMultimap<Long,AdgroupDO> adgroupMap = Multimaps.index(ImmutableSet.copyOf(list),pdoGroupByCustIdFunction);
+
+println adgroupMap
+
+
