ee24b798be84651103c802c0c43db124837e69db
lilongthinker <lilongthinker@163.com>
2022-10-20 17:16:06 +0800

    add log

--- nodejs/midway-hello/hello-midway/src/service/user.ts
+++ nodejs/midway-hello/hello-midway/src/service/user.ts
@@ -1,14 +1,19 @@
-import { Provide } from '@midwayjs/decorator';
+import { Provide, Inject } from '@midwayjs/decorator';
 import { IUserOptions } from '../interface';
-
+import { ILogger } from '@midwayjs/logger';
 @Provide()
 export class UserService {
+  @Inject()
+  logger: ILogger;
+
   async getUser(options: IUserOptions) {
+    this.logger.info('options: '+JSON.stringify(options));
+
     return {
       uid: options.uid,
       username: 'mockedName',
       phone: '12345678901',
-      email: 'xxx.xxx@xxx.com',
+      email: 'midway-mock@163.com',
     };
   }
 }
